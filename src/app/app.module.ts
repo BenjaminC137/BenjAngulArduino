11948938d56112fb665484cae597c95d4789c42e
Benjamin <benjaminsalasek@Athena.local>
2018-07-20 15:29:39 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QaComponent } from './qa/qa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ProjectDetailsComponent,
+    NavBarComponent,
+    QaComponent
   ],
   imports: [
     BrowserModule
